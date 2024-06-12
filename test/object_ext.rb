b9d72a17648647b50837e6f647ae1823896c6adb
buty4649 <buty4649@gmail.com>
2024-06-12 23:53:05 +0900

    Add to_json method to Object class with tests

---
+++ test/object_ext.rb
@@ -0,0 +1,11 @@
+assert('Object#to_json') do
+  assert_equal "null", nil.to_json
+  assert_equal "false", false.to_json
+  assert_equal "true", true.to_json
+  assert_equal "100", 100.to_json
+  assert_equal "0.25", 0.25.to_json
+  assert_equal %("mrb-yyjson"), "mrb-yyjson".to_json
+  assert_equal %("JSON"), :JSON.to_json
+  assert_equal %([true,1,"mrb-yyjson"]), [true, 1, "mrb-yyjson"].to_json
+  assert_equal %({"mrb":"yyjson","foo":123,"JSON":"json"}), {"mrb" => "yyjson", foo: 123, JSON:"json"}.to_json
+end
